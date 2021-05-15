2ffab1762007f9ffdda4ffc4f65043d157162dad
Vahit Keskin <36104238+Vahit11@users.noreply.github.com>
2021-05-15 20:17:56 +0300

    vahitkeskin

--- app/src/main/java/com/vahitkeskin/kotlinshoppingapp/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/vahitkeskin/kotlinshoppingapp/adapter/CategoryAdapter.kt
@@ -1,8 +1,11 @@
 package com.vahitkeskin.kotlinshoppingapp.adapter
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Callback
@@ -10,18 +13,34 @@ import com.squareup.picasso.Picasso
 import com.vahitkeskin.kotlinshoppingapp.R
 import com.vahitkeskin.kotlinshoppingapp.model.Categories
 import kotlinx.android.synthetic.main.item_category.view.*
-import java.lang.Exception
+
 
 class CategoryAdapter(
     private val categoryArray: ArrayList<Categories>, private val categoryListener: CategoryListener
 ) :
     RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>(), CategoryClickListener {
 
-    inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    private val categoryBorderColors: Array<String> = arrayOf(
+        "#403e91",
+        "#8ee81f",
+        "#bd7ad5",
+        "#973b5a",
+        "#1cd678",
+        "#bf2e4d",
+        "#0a8afd",
+        "#9ce1c8",
+        "#f09d94",
+        "#fe85fa",
+        "#8586a3",
+        "#84dcb8"
+    )
+
+    inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val relativeLayout: RelativeLayout = view.findViewById(R.id.rlCategory)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        //val view = DataBindingUtil.inflate<ItemCategoryBinding>(inflater, R.layout.item_category, parent, false)
         val view = inflater.inflate(R.layout.item_category, parent, false)
         return CategoryViewHolder(view)
     }
@@ -47,6 +66,15 @@ class CategoryAdapter(
                 }
 
             })
+
+        val gd = GradientDrawable()
+        gd.cornerRadius = 10f
+        gd.setStroke(
+            15,
+            Color.parseColor(categoryBorderColors[position % 6])
+        )
+        holder.relativeLayout.setBackgroundDrawable(gd)
+
     }
 
     override fun getItemCount(): Int {
@@ -63,7 +91,7 @@ class CategoryAdapter(
 
 
     interface CategoryListener {
-        fun onCategoryClickListener(position: Int,categoryName: String)
+        fun onCategoryClickListener(position: Int, categoryName: String)
     }
 
 }
