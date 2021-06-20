8fc9041a929ee997c21301a0f49629e1b61bff99
Vahit Keskin <36104238+Vahit11@users.noreply.github.com>
2021-06-20 22:37:18 +0300

    vahitkeskin
    vahitkeskin

--- app/src/main/java/com/vahitkeskin/kotlinshoppingapp/util/UtilStrings.kt
+++ app/src/main/java/com/vahitkeskin/kotlinshoppingapp/util/UtilStrings.kt
@@ -2,8 +2,8 @@ package com.vahitkeskin.kotlinshoppingapp.util
 
 object UtilStrings {
     const val BASE_URL = "https://raw.githubusercontent.com/"
-    const val SHOPPING_URL = "Vahit11/kotlin-shopping-app/master/shopping.json"
-    const val CATEGORY_URL = "Shopping/categories.json"
+    const val SHOPPING_URL = "Vahit11/kotlin-shopping-app/master/Shopping/shopping.json"
+    const val CATEGORY_URL = "Vahit11/kotlin-shopping-app/master/Shopping/categories.json"
     const val DB_NAME = "shoppingDatabase"
     const val PREFERENCES_TIME = "preferences_time"
 }
